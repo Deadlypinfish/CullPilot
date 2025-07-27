94abe6584917bdd7a2a169a9e36059470a2b83d6
Alex <alexferraro35@gmail.com>
2025-07-27 19:47:35 -0400

    removed references to popup.[js|ts]

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         content: resolve(__dirname, 'src/content.ts'),
-        popup: resolve(__dirname, 'src/popup.ts') // just the JS
       },
       output: {
         entryFileNames: chunk => {
           if (chunk.name === 'background') return 'background.js';
           if (chunk.name === 'content') return 'content.js';
-          if (chunk.name === 'popup') return 'popup.js';
           return '[name].js';
         }
       }
